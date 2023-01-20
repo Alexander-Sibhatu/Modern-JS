4a48f86ed6a0d15b83d184030cddec65c8324eb3
Alexander <sibhatualexander5@gmail.com>
2023-01-20 07:02:31 +0100

    Displaying the length of an array

--- Simplicity/scriptSimple.js
+++ Simplicity/scriptSimple.js
@@ -1,7 +1,11 @@
 console.log("Hello World");
 let fruits = []
 fruits = ["Banana", "Kiwi", "Orange", "Apple"]
-console.log(fruits[0]);
+//console.log(fruits[0]);
+
+console.log(fruits.length);
+
+
 
 
 //console.log(fruits[fruits.length-1]);
