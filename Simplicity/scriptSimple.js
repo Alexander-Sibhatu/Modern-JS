f72b64e23f32a96bc441ddef1e751106d6b75270
Alexander <sibhatualexander5@gmail.com>
2023-01-20 07:03:12 +0100

    Displaying the last element of an array

--- Simplicity/scriptSimple.js
+++ Simplicity/scriptSimple.js
@@ -3,10 +3,8 @@ let fruits = []
 fruits = ["Banana", "Kiwi", "Orange", "Apple"]
 //console.log(fruits[0]);
 
-console.log(fruits.length);
+//console.log(fruits.length);
 
 
-
-
-//console.log(fruits[fruits.length-1]);
+console.log(fruits[fruits.length-1]);
 
