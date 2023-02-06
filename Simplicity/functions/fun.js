8aeffc9562bb8d273cba6c68fe602e6ff5a41793
Alexander <sibhatualexander5@gmail.com>
2023-02-06 23:03:47 +0100

    map array method

--- Simplicity/functions/fun.js
+++ Simplicity/functions/fun.js
@@ -13,8 +13,22 @@
 const number = [1, 2, 3, 4, 5, 6];
 const squareNumber = [];
 
+/* forEach array Method
+
 number.forEach(function(value) {
     squareNumber.push(value * value);
 });
 
 console.log(squareNumber);
+*/
+
+
+/* map array method */
+
+const squareArray = number.map(function(value) {
+    return value*value;
+});
+
+console.log(squareArray);
+
+
