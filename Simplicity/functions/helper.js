68415cee997bede941e300ddc35b2369eda44569
Alexander <sibhatualexander5@gmail.com>
2023-02-09 20:18:19 +0100

    student class moved to helper file to be imported

---
+++ Simplicity/functions/helper.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor (age, nationality) {
+        this.age = age;
+        this.nationality = nationality;
+
+    }
+
+    printInfo () {
+        console.log(`age: ${this.age} nationality: ${this.nationality}`);
+    }
+}
