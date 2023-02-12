e2c29a3e9bc951bd04af431717e156bbd7b3ab8f
Alexander <sibhatualexander5@gmail.com>
2023-02-12 10:30:28 +0100

    object inheriting a class

---
+++ Simplicity/functions/class.js
@@ -0,0 +1,11 @@
+// importing student from helper.js
+import { Student } from "./helper.js";
+
+
+const Alex = new Student("Alexander", 30, "Geez"); 
+const Anisul = new Student("Anisul", 32, "Bangeladesh");
+const Meron = new Student ("Meron", 26, "Ethiopia");
+
+Alex.printInfo();
+Anisul.printInfo();
+Meron.printInfo();
